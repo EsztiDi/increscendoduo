25ce7a1342a8089d12cd1d717893e9579ab2deeb
EsztiDi <esztit@gmail.com>
2020-06-17 14:07:59 +0100

    Updated slideshow

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     if (prevScrollPos > currentScrollPos) {
       $("body > header").css("top", "0");
     } else {
-      $("body > header").css("top", "-21vw");
+      $("body > header").css("top", "-50%");
     }
     prevScrollPos = currentScrollPos;
   })
@@ -16,14 +16,26 @@ $(document).ready(function() {
   slideShow();
   function slideShow() {
     var slides = $(".slides");
-    slides.each(function () {
-      $(this).fadeOut("slow");
-    });
+
+    slides.eq(slideIndex - 1).fadeTo(800, 0);
+    slides.eq(slideIndex - 1).css({"left": "20%", "width": "60%", "filter": ""});
+    slides.each(function(index){
+      $(this).css("z-index", -index);
+    })
+
     slideIndex++;
     if (slideIndex > slides.length) {
       slideIndex = 1;
     }
-    slides.eq(slideIndex - 1).fadeIn("slow");
+    if (slideIndex == slides.length) {
+      slides.eq(0).fadeTo(1200, 0.4);
+      slides.eq(0).css({"left": "0", "width": "100%", "filter": "blur(3px)", "z-index": -slideIndex});
+    } else {
+      slides.eq(slideIndex).fadeTo(1200, 0.4);
+      slides.eq(slideIndex).css({"left": "0", "width": "100%", "filter": "blur(3px)"});
+    }
+    slides.eq(slideIndex - 1).fadeTo(1500, 1);
+    slides.eq(slideIndex - 1).css({"left": "20%", "width": "60%", "filter": ""});
     setTimeout(slideShow, 7000);
   }
 
