3c232dd30614ff79fe93a54b37b6d8a58d7da0ff
EsztiDi <esztit@gmail.com>
2020-06-24 14:39:45 +0100

    White header, 2 photos

--- scripts/main.js
+++ scripts/main.js
@@ -1,82 +1,69 @@
 $(document).ready(function() {
   
-  if (window.matchMedia("(min-width: 641px)").matches) {
-    var prevScrollPos = window.pageYOffset;
-    $(window).on("scroll", function(){
-      var currentScrollPos = window.pageYOffset;
+  // if (window.matchMedia("(min-width: 641px)").matches) {
+  //   var prevScrollPos = window.pageYOffset;
+  //   $(window).on("scroll", function(){
+  //     var currentScrollPos = window.pageYOffset;
 
-      if (prevScrollPos > currentScrollPos) {
-        $("body > header").css("top", "0");
-      } else {
-        $("body > header").css("top", "-13rem");
-      }
-      prevScrollPos = currentScrollPos;
-    })
-  }
+  //     if (prevScrollPos > currentScrollPos) {
+  //       $("body > header").css("top", "0");
+  //     } else {
+  //       $("body > header").css("top", "-13rem");
+  //     }
+  //     prevScrollPos = currentScrollPos;
+  //   })
+  // }
 
-  var slideIndex = 0;
-  $(".container").fadeIn(1000);
-  slideShow();
-  function slideShow() {
-    var slides = $(".slides");
+  if (window.matchMedia("(max-width: 640px)").matches) {
+    var slideIndex = 0;
+    // $(".container").fadeIn(1000);
+    slideShow();
+    function slideShow() {
+      var slides = $(".slides");
 
-    // slides.eq(slideIndex - 1).fadeTo(1000, 0);
-    slides.each(function(index){
-      // $(this).css("z-index", -index);
-      $(this).fadeOut(1000);
-    });
+      slides.each(function(index){
+        $(this).fadeOut(1000);
+      });
 
-    slideIndex++;
-    if (slideIndex > slides.length) {
-      slideIndex = 1;
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1;}
+      slides.eq(slideIndex - 1).fadeIn(1000);
+      setTimeout(slideShow, 7000);
     }
-    // if (slideIndex == slides.length) {
-    //   slides.eq(0).fadeTo(500, 0.4);
-    //   slides.eq(0).css({"left": "0", "width": "100%", "filter": "blur(3px)", "z-index": -slideIndex});
-    // } else {
-    //   slides.eq(slideIndex).fadeTo(500, 0.4);
-    //   slides.eq(slideIndex).css({"left": "0", "width": "100%", "filter": "blur(3px)"});
-    // }
-    slides.eq(slideIndex - 1).fadeIn(1000);
-    // slides.eq(slideIndex - 1).fadeTo(500, 1);
-    // slides.eq(slideIndex - 1).css({"left": "20%", "width": "60%", "filter": ""});
-    setTimeout(slideShow, 7000);
   }
 
   $(".card").each(function(index) {
     $(this).on("click", function () {
-      $(".modal").fadeIn();
       currentSlide(index + 1);
       if (window.matchMedia("(pointer: fine)").matches) {
-        $("body").addClass("modal-open").css("overflow-y", "hidden");
-        $("body > header").addClass("modal-open");
+        $("body").addClass("modal-open");
+        // $("body > header").addClass("modal-open");
       }
+      $(".modal").fadeIn();
     })
   });
 
-  $(".close").on("click", function() {
+  function hideModal() {
     $(".modal").fadeOut();
     if (window.matchMedia("(pointer: fine)").matches) {
-      $("body").removeClass("modal-open").css("overflow-y", "");
-      $("body > header").removeClass("modal-open");
+      setTimeout(function() {
+        $("body").removeClass("modal-open");
+      }, 300);
+      // $("body > header").removeClass("modal-open");
     }
-  })
+  }
+
+  $(".close").on("click", function() {
+    hideModal();
+  });
   $(window).click(function(event) {
     if ($(event.target).hasClass("modal-content")) {
-      $(".modal").fadeOut();
-      if (window.matchMedia("(pointer: fine)").matches) {
-        $("body").removeClass("modal-open").css("overflow-y", "");
-        $("body > header").removeClass("modal-open");
-      }
+      hideModal();
     }
   });
   $(document).keydown(function(event) {
     if (event.keyCode == 27) {
-      $(".modal").fadeOut();
-      if (window.matchMedia("(pointer: fine)").matches) {
-        $("body").removeClass("modal-open").css("overflow-y", "");
-        $("body > header").removeClass("modal-open");
-      }
+      hideModal();
     }
     if (event.keyCode == 37) {
       plusSlides(-1);
@@ -93,11 +80,11 @@ $(document).ready(function() {
     plusSlides(1);
   });
 
-  $(".thumbnail").each(function (index) {
-    $(this).on("click", function () {
-      currentSlide(index + 1);
-    });
-  });
+  // $(".thumbnail").each(function (index) {
+  //   $(this).on("click", function () {
+  //     currentSlide(index + 1);
+  //   });
+  // });
 
   var modalSlideIndex = 1;
   modalSlides(modalSlideIndex);
@@ -122,60 +109,78 @@ $(document).ready(function() {
 
   $("header .fa-bars, header .fa-times").on("click", function () {
     $("body > header").toggleClass("show1");
-    $("nav ul, .social").toggleClass("show2");
+    setTimeout(function() {
+      $("nav ul, .social").toggleClass("show2");
+    }, 100);
     $("header .fa-bars, header .fa-times").toggle(500);
   });
 
   gsap.registerPlugin(ScrollTrigger);
 
   ScrollTrigger.defaults({
-    start: "-10% bottom",
-    // start: "top 90%",
-    // end: "50% bottom",
-  });
-  // gsap.defaults({
-  //   duration: 0.7,
-  //   opacity: 0,
-  // });
-
-  gsap.from(".us", { y: innerWidth * 0.1, opacity: 0, duration: 0.7, scrollTrigger: ".us" });
-  gsap.from(".next", { y: innerWidth * 0.1, opacity: 0, duration: 0.7, scrollTrigger: ".next" });
-  gsap.from(".contact", { y: innerWidth * 0.1, opacity: 0, duration: 0.7, scrollTrigger: {trigger: ".contact", start: "-50% bottom"}});
-  
-  gsap.from(".email", { y: innerWidth * 0.1, opacity: 0, duration: 0.7, scrollTrigger: ".email" });
-  gsap.from(".phone", { y: innerWidth * 0.1, opacity: 0, duration: 0.7, scrollTrigger: ".phone" });
-  gsap.from(".instagram", { y: innerWidth * 0.1, opacity: 0, duration: 0.7, scrollTrigger: ".instagram" });
-  gsap.from(".facebook", { y: innerWidth * 0.1, opacity: 0, duration: 0.7, scrollTrigger: ".facebook" });
-  gsap.from(".contacts h4", { opacity: 0, duration: 0.7, scrollTrigger: ".contacts h4" });
-
-  gsap.from(".videos", { opacity: 0, duration: 0.7, scrollTrigger: ".videos" });
-
-  gsap.from(".duo .img-container", { opacity: 0, duration: 0.7, scrollTrigger: ".duo" });
-  gsap.from(".duo h3", { opacity: 0, duration: 0.7, scrollTrigger: ".duo" });
-  $(".duo p").each(function() {
-    gsap.from(this, { y: innerWidth * 0.1, opacity: 0, duration: 0.7, scrollTrigger: this });
+    start: "10% bottom",
   });
 
-  gsap.from(".flora .img-container", { opacity: 0, duration: 0.7, scrollTrigger: ".flora" });
-  gsap.from(".flora h3", { opacity: 0, duration: 0.7, scrollTrigger: ".flora" });
-  $(".flora p").each(function() {
-    gsap.from(this, { y: innerWidth * 0.1, opacity: 0, duration: 0.7, scrollTrigger: this });
+  ScrollTrigger.batch(".cards div", {
+    onEnter: (elements, triggers) => {
+      gsap.from(elements, { y: 70, opacity: 0, duration: 0.6, stagger: 0.2 });
+    },
+    once: true,
   });
 
-  gsap.from(".daniel .img-container", { y: innerWidth * 0.1, opacity: 0, duration: 0.7, scrollTrigger: ".daniel" });
-  gsap.from(".daniel h3", { y: innerWidth * 0.1, opacity: 0, duration: 0.7, scrollTrigger: ".daniel" });
-  $(".daniel p").each(function() {
-    gsap.from(this, { y: innerWidth * 0.1, opacity: 0, duration: 0.7, scrollTrigger: this });
+  ScrollTrigger.batch(".flora h3, .flora div", {
+    onEnter: (elements, triggers) => {
+      gsap.from(elements, { opacity: 0, duration: 0.8, stagger: 0.2 });
+    },
+    once: true,
+  });
+  ScrollTrigger.batch(".flora p", {
+    onEnter: (elements, triggers) => {
+      gsap.from(elements, { y: 70, opacity: 0, duration: 0.6, stagger: 0.2 });
+    },
+    once: true,
   });
 
-  gsap.set(".card", { y: innerWidth * 0.1, opacity: 0 });
+  ScrollTrigger.batch(".daniel h3, .daniel div", {
+    onEnter: (elements, triggers) => {
+      gsap.from(elements, { opacity: 0, duration: 0.8, stagger: 0.2 });
+    },
+    once: true,
+  });
+  ScrollTrigger.batch(".daniel p", {
+    onEnter: (elements, triggers) => {
+      gsap.from(elements, { y: 70, opacity: 0, duration: 0.6, stagger: 0.2 });
+    },
+    once: true,
+  });
+
+  ScrollTrigger.batch(".rep h3, .rep div", {
+    onEnter: (elements, triggers) => {
+      gsap.from(elements, { opacity: 0, duration: 0.8, stagger: 0.2 });
+    },
+    once: true,
+  });
+  ScrollTrigger.batch(".rep p", {
+    onEnter: (elements, triggers) => {
+      gsap.from(elements, { y: 70, opacity: 0, duration: 0.6, stagger: 0.2 });
+    },
+    once: true,
+  });
+
+  // gsap.set(".card", { y: 100, opacity: 0 });
   ScrollTrigger.batch(".card", {
     onEnter: (elements, triggers) => {
-      gsap.to(elements, { y: 0, opacity: 1, duration: 0.7, stagger: 0.2 });
-      // gsap.from(elements, { y: innerWidth * 0.1, opacity: 0, duration: 0.7, stagger: 0.2 });
+      // gsap.to(elements, { y: 0, opacity: 1, duration: 0.6, stagger: 0.2 });
+      gsap.from(elements, { y: 70, opacity: 0, duration: 0.6, stagger: 0.2 });
     },
-    start: "-50% bottom",
     once: true
+  });
+
+  ScrollTrigger.batch(".music h3, .music div", {
+    onEnter: (elements, triggers) => {
+      gsap.from(elements, { opacity: 0, duration: 0.8, stagger: 0.2 });
+    },
+    once: true,
   });
 
 
