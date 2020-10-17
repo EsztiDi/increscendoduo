80776c48dab6402c2a12ca934032a7a6222f941a
EsztiDi <esztit@gmail.com>
2020-10-17 12:16:55 +0100

    Added error handling for photos

--- photos.php
+++ photos.php
@@ -12,17 +12,17 @@ include './partials/head.php';
   <main>
     <h2>Photos</h2>
     <section class="photos cushycms" id="Photos">
-      <img class="tall" src="./images/2.jpg" alt="In Crescendo Duo 2">
-      <img src="./images/3.jpg" alt="Daniel Acosta 1">
-      <img class="tall" src="./images/9.jpg" alt="Flora Acosta 4">
-      <img src="./images/5.jpg" alt="Daniel Acosta 3">
-      <img class="tall" src="./images/8.jpg" alt="Flora Acosta 3">
-      <img class="tall" src="./images/6.jpg" alt="Flora Acosta 1">
-      <img class="tall" src="./images/11.jpg" alt="Daniel Acosta 5">
-      <img class="tall" src="./images/4.jpg" alt="Daniel Acosta 2">
-      <img class="tall" src="./images/7.jpg" alt="Flora Acosta 2">
-      <img src="./images/1.jpg" alt="In Crescendo Duo 1">
-      <img src="./images/10.jpg" alt="Daniel Acosta 4">
+      <img class="tall" src="./images/2.jpg" alt="In Crescendo Duo 2" onerror="this.style.display='none'">
+      <img src="./images/3.jpg" alt="Daniel Acosta 1" onerror="this.style.display='none'">
+      <img class="tall" src="./images/9.jpg" alt="Flora Acosta 4" onerror="this.style.display='none'">
+      <img src="./images/5.jpg" alt="Daniel Acosta 3" onerror="this.style.display='none'">
+      <img class="tall" src="./images/8.jpg" alt="Flora Acosta 3" onerror="this.style.display='none'">
+      <img class="tall" src="./images/6.jpg" alt="Flora Acosta 1" onerror="this.style.display='none'">
+      <img class="tall" src="./images/11.jpg" alt="Daniel Acosta 5" onerror="this.style.display='none'">
+      <img class="tall" src="./images/4.jpg" alt="Daniel Acosta 2" onerror="this.style.display='none'">
+      <img class="tall" src="./images/7.jpg" alt="Flora Acosta 2" onerror="this.style.display='none'">
+      <img src="./images/1.jpg" alt="In Crescendo Duo 1" onerror="this.style.display='none'">
+      <img src="./images/10.jpg" alt="Daniel Acosta 4" onerror="this.style.display='none'">
     </section>
     <div class="modal">
       <div class="modal-content">
